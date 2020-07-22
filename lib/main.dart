ca37077b49176135a2e2fc384c55c7a2d341524c
Senaru <s3696653@student.rmit.edu.au>
2020-07-22 15:08:17 +1000

    Using Container Widgets and SafeArea

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,16 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Container(
+            height: 100,
+            width: 100,
+            margin: EdgeInsets.fromLTRB(30, 10, 50, 20),
+            padding: EdgeInsets.all(30),
+            color: Colors.white,
+            child: Text("Hello"),
+          ),
+        ),
       ),
     );
   }
