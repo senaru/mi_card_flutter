3004e53b9f99f209eb127340d9f8af47a3e72952
Senaru <s3696653@student.rmit.edu.au>
2020-07-24 23:17:38 +1000

    Using ListTiles

--- lib/main.dart
+++ lib/main.dart
@@ -38,43 +38,29 @@ class MyApp extends StatelessWidget {
               Card(
                 color: Colors.white,
                 margin: EdgeInsets.symmetric(vertical: 10, horizontal: 30),
-                child: Padding(
-                  padding: EdgeInsets.all(10),
-                  child: Row(children: <Widget>[
-                    Icon(
-                      Icons.phone,
-                      color: Colors.teal,
-                    ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Text(
-                      "+61 406 807 279",
-                      style:
-                          TextStyle(color: Colors.teal.shade900, fontSize: 20),
-                    ),
-                  ]),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    "+61 406 807 279",
+                    style: TextStyle(color: Colors.teal.shade900, fontSize: 20),
+                  ),
                 ),
               ),
               Card(
                 color: Colors.white,
                 margin: EdgeInsets.symmetric(vertical: 10, horizontal: 30),
-                child: Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Row(children: <Widget>[
-                    Icon(
-                      Icons.email,
-                      color: Colors.teal,
-                    ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Text(
-                      "Lonathtech@gmail.com",
-                      style:
-                          TextStyle(color: Colors.teal.shade900, fontSize: 20),
-                    ),
-                  ]),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    "Lonathtech@gmail.com",
+                    style: TextStyle(color: Colors.teal.shade900, fontSize: 20),
+                  ),
                 ),
               ),
             ],
