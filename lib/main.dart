cd051d32284facfe18f9a86cbf6313520e3849fb
Senaru <s3696653@student.rmit.edu.au>
2020-07-24 23:12:17 +1000

    Flutter Cards and Padding

--- lib/main.dart
+++ lib/main.dart
@@ -35,41 +35,47 @@ class MyApp extends StatelessWidget {
                     fontSize: 26,
                     fontWeight: FontWeight.w700),
               ),
-              Container(
+              Card(
                 color: Colors.white,
-                padding: EdgeInsets.all(10),
                 margin: EdgeInsets.symmetric(vertical: 10, horizontal: 30),
-                child: Row(children: <Widget>[
-                  Icon(
-                    Icons.phone,
-                    color: Colors.teal,
-                  ),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Text(
-                    "+61 406 807 279",
-                    style: TextStyle(color: Colors.teal.shade900, fontSize: 20),
-                  ),
-                ]),
+                child: Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Row(children: <Widget>[
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      "+61 406 807 279",
+                      style:
+                          TextStyle(color: Colors.teal.shade900, fontSize: 20),
+                    ),
+                  ]),
+                ),
               ),
-              Container(
+              Card(
                 color: Colors.white,
-                padding: EdgeInsets.all(10),
                 margin: EdgeInsets.symmetric(vertical: 10, horizontal: 30),
-                child: Row(children: <Widget>[
-                  Icon(
-                    Icons.email,
-                    color: Colors.teal,
-                  ),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Text(
-                    "Lonathtech@gmail.com",
-                    style: TextStyle(color: Colors.teal.shade900, fontSize: 20),
-                  ),
-                ]),
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Row(children: <Widget>[
+                    Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      "Lonathtech@gmail.com",
+                      style:
+                          TextStyle(color: Colors.teal.shade900, fontSize: 20),
+                    ),
+                  ]),
+                ),
               ),
             ],
           ),
