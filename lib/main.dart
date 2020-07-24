81ee64937d58937325951f8c8a70a04e1fb01771
Senaru <s3696653@student.rmit.edu.au>
2020-07-24 23:02:19 +1000

    Adding Icon widgets

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               CircleAvatar(
                 radius: 50,
@@ -27,11 +28,49 @@ class MyApp extends StatelessWidget {
                   fontFamily: 'Pacifico',
                 ),
               ),
-              Text("MOBILE DEVELOPER",
-                  style: TextStyle(
-                      color: Colors.white54,
-                      fontSize: 26,
-                      fontWeight: FontWeight.w700)),
+              Text(
+                "MOBILE DEVELOPER",
+                style: TextStyle(
+                    color: Colors.white54,
+                    fontSize: 26,
+                    fontWeight: FontWeight.w700),
+              ),
+              Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(10),
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+                child: Row(children: <Widget>[
+                  Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    "+61 406 807 279",
+                    style: TextStyle(color: Colors.teal.shade900, fontSize: 20),
+                  ),
+                ]),
+              ),
+              Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(10),
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+                child: Row(children: <Widget>[
+                  Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    "Lonathtech@gmail.com",
+                    style: TextStyle(color: Colors.teal.shade900, fontSize: 20),
+                  ),
+                ]),
+              ),
             ],
           ),
         ),
